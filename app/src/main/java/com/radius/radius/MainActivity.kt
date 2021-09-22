82319d1ea2f04bc11ebbf676271fcff65e95a0f1
sreekuttancj <sreekuttancj321@gmail.com>
2021-09-22 11:52:06 +0530

    updated main activity layout and class

--- app/src/main/java/com/radius/radius/MainActivity.kt
+++ app/src/main/java/com/radius/radius/MainActivity.kt
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.radius.data.viewmodel.FacilityViewModel
+import com.radius.radius.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val facilityViewModel by viewModels<FacilityViewModel>()
-
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(activityMainBinding.toolbar.toolbarMain)
+        setContentView(activityMainBinding.root)
 
         facilityViewModel.getFacilityInfo()
     }
